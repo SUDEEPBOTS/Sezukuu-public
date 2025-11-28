e7dcf0a8ff8e49ab42ad316a00fa2be2080ec6b1
𝐒𝐔𝐃𝐄𝐄𝐏𝐁𝐎𝐓𝐒 <sudeeptelegram@gmail.com>
2025-11-28 10:27:35 +0530

    Update PublicUser.js

--- models/PublicUser.js
+++ models/PublicUser.js
@@ -1,9 +1,16 @@
 import mongoose from "mongoose";
 
-const PublicUserSchema = new mongoose.Schema({
-  username: { type: String, unique: true },
-  createdAt: { type: Date, default: Date.now },
-});
+const PublicUserSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    createdAt: { type: Date, default: Date.now },
+
+    // Extra future fields
+    banned: { type: Boolean, default: false },
+    bots: { type: Number, default: 0 },
+  },
+  { versionKey: false }
+);
 
 export default mongoose.models.PublicUser ||
   mongoose.model("PublicUser", PublicUserSchema);
